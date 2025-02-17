897c533a968175e4648d1619b834c2a9d42bc22c
cusbert <mindychoi20@gmail.com>
2025-02-18 00:05:59 +0900

    first commit

---
+++ src/main/java/com/example/web/SampleController.java
@@ -0,0 +1,23 @@
+package com.example.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("web")
+public class SampleController {
+
+    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+
+    @RequestMapping("doA")
+    public void doA() {
+        logger.info("doA called...");
+    }
+
+    @RequestMapping("doB")
+    public void doB() {
+        logger.info("doB called...");
+    }
+}
